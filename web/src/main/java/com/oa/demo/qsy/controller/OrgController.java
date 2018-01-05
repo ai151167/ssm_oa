70e14dfd21879a1c0b29824e96980acc10f6c2fe
qinshuangyang <you@example.com>
2018-01-05 16:40:51 +0800

    org

--- web/src/main/java/com/oa/demo/qsy/controller/OrgController.java
+++ web/src/main/java/com/oa/demo/qsy/controller/OrgController.java
@@ -1,11 +1,11 @@
 package com.oa.demo.qsy.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.oa.demo.qsy.pojo.SysOrg;
 import com.oa.demo.qsy.service.IOrgService;
@@ -17,9 +17,10 @@ public class OrgController {
 	@Autowired
 	private IOrgService orgServiceImpl;
 	
-	@ResponseBody
 	@RequestMapping("/queryOrgList")
-	public List<SysOrg> queryOrgList(){
-		return orgServiceImpl.queryOrgList();
+	public String queryOrgList(Map<String, Object> map){
+		List<SysOrg> orgList = orgServiceImpl.queryOrgList();
+		map.put("list", orgList);
+		return "page/org/orgList";
 	}
 }
